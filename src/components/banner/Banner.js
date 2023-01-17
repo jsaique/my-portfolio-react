bd2389ce705f8a20b03f2e8426e122893cd8be63
jsaique <jsaique11@gmail.com>
2023-01-16 23:45:23 -0500

    Fixed navbar design

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -1,6 +1,6 @@
 import React from "react";
 import LeftBanner from "./LeftBanner";
-import { bannerImg } from "../../assets/index";
+import RightBanner from "./RightBanner";
 
 const Banner = () => {
   return (
@@ -9,15 +9,7 @@ const Banner = () => {
       className="w-full pt-10 pb-20 flex items-center border-b-[1px] font-ttileFont border-b-darkColor"
     >
       <LeftBanner />
-
-      <div className="w-1/2 flex justify-center items-center relative">
-        <img
-          className="w-[500px] h-[680px] z-10"
-          src={bannerImg}
-          alt="John's picture"
-        />
-        <div className="absolute bottom-0 w-[500px] h-[500px] bg-gradient-to-r from-[#1e2024] to-[#202327] shadow-shadowOne flex justify-center items-center"></div>
-      </div>
+      <RightBanner />
     </section>
   );
 };
